7738675157a23544195554116f6a387dae982cf8
Filip Starý <filipstary@email.com>
2026-09-03 09:29:35 +0200

    Use hyphen in app title

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'baryton-bonit-v16';
+const CACHE = 'baryton-bonit-v17';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './config.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
